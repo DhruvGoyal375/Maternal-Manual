8b1d13aa6faa15e663d73ca0cfb2d630b2ebc59f
DhruvGoyal375 <dhruvgoyal375@gmail.com>
2023-03-31 23:10:36 +0530

    Second Commit

---
+++ src/components/pages/Trimester3.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import './newIndex.css'
+const Trimester3 = () => {
+
+    return (
+        <>
+        <div className='outer-container'>
+            <div className='trimester-info'>
+                <h1 className='info-head'>Trimester III</h1>
+                <p className='info-para'>The third trimester of pregnancy begins at week 29 and ends with childbirth. During this time, the fetus continues to grow and develop, and the mother's body undergoes several changes in preparation for delivery. The baby's organs are fully developed, and the baby gains weight and size rapidly. The mother's uterus expands and the baby may settle into a head-down position in preparation for birth. The mother may experience shortness of breath, difficulty sleeping, and back pain as the baby puts pressure on her internal organs. Prenatal checkups become more frequent during the third trimester to monitor the baby's position, growth, and overall health. The mother may also undergo various tests to prepare for delivery, such as Group B Streptococcus screening. It's important for the mother to prepare for childbirth, including creating a birth plan, packing a hospital bag, and attending childbirth education classes.</p>
+            </div>
+        </div>
+
+        <div className='do-dont-container'>
+            <div className='do-container'>
+                <h1 className='head'>Do's</h1>
+                <li className='text'>Keep track of fetal movement</li>
+                <li className='text'>Watch your weight</li>
+                <li className='text'>Keep up with the pregnancy safe exercises</li>
+                <li className='text'>Buy baby gear</li>
+                <li className='text'>Stay active and keep moving, with your practitioners okay and while following a few safety precautions</li>
+                <li className='text'>Schedule your third trimester checkups</li>
+                <li className='text'>Take a hospital tour </li>
+                <li className='text'>Choose your baby's pediatrician?</li>
+                <li className='text'>Get educated</li>
+                <li className='text'>Prepare to breastfeed</li>
+                <li className='text'>Learn about stages of labour</li>
+                <li className='text'>Consider how you'd like to manage labor pain</li>
+                <li className='text'>Check your birth plan</li>
+                <li className='text'>Set up your nursery</li>
+                <li className='text'>Commemorate your baby bump</li>
+                <li className='text'>Stock your fridge for the first few weeks post delivery</li>
+                <li className='text'>Start with financial Planning</li>
+                <li className='text'>Pack your hospital bag, a few comforts from home you'd like in the hospital</li>
+                <li className='text'>Arrange for  cord blood banking</li>
+                <li className='text'>Learn what happens after birth</li>
+                <li className='text'>Prepare for your baby's first year</li>
+            </div>
+            <div className='dont-container'>
+                <h1 className='head'>Don'ts</h1>
+                <li className='text'>Travelling</li>
+                <li className='text'>Lying on your back</li>
+                <li className='text'>Hot tubs and saunas</li>
+                <li className='text'>Exercising in warm temperatures</li>
+                <li className='text'>Consumption of alcohol</li>
+                <li className='text'>Raw or undercooked foods</li>
+                <li className='text'>Unpasteurized dairy and juices</li>
+                <li className='text'>Smoking and other drugs</li>
+                <li className='text'>Extreme physical exertion</li>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Trimester3
